cca40b25b366aed7dcf503400fe9915f94c29bb7
jaoviitor <joaoviiitorvieira@gmail.com>
2024-05-29 21:59:58 -0300

    add api connection

---
+++ src/js/login.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('.login-form');
+
+    form.addEventListener('submit', async (event) => {
+        event.preventDefault();
+
+        const emailInput = form.querySelector('input[type="email"]');
+        const passwordInput = form.querySelector('input[type="password"]');
+
+        function showModal(token, username) {
+            const modal = document.getElementById("myModal");
+            const tokenSpan = document.getElementById("sessionToken");
+            const userSpan = document.getElementById("username")
+            userSpan.textContent = username;
+            tokenSpan.textContent = token; // Define o token de sessão no modal
+            modal.style.display = "block"; // Exibe o modal
+        }
+
+        document.querySelector(".close").addEventListener("click", function() {
+            document.getElementById("myModal").style.display = "none";
+        });
+
+        const userData = {
+            email: emailInput.value,
+            senha: passwordInput.value
+        };
+
+        try {
+            const response = await fetch('http://localhost:8090/api/usuario/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(userData)
+            });
+
+            if (!response.ok) {
+                const errorData = await response.json();
+                throw new Error(errorData.message || 'Erro ao fazer login');
+            }
+
+            const data = await response.json();
+            console.log('Login bem-sucedido:', data);
+            const mockSessionToken = data.jwt;
+            const mockUsername = data.nome;
+            showModal(mockSessionToken, mockUsername);
+        } catch (error) {
+            console.error('Erro:', error.message);
+        }
+    });
+});
--- src/js/register.js
+++ src/js/register.js
@@ -1,6 +1,82 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const datePicker = document.getElementById("data");
-    datePicker.addEventListener("click", function() {
-        this.type = "date";
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('.login-form');
+
+
+    function showLoader() {
+        const loader = document.getElementById("loader");
+        loader.style.display = 'block'; // Exibir o loader
+    }
+
+    function hideLoader() {
+        const loader = document.getElementById("loader");
+        loader.style.display = 'none'; // Ocultar o loader
+    }
+
+    function showModal() {
+        const modal = document.getElementById("myModal");
+        modal.style.display = "block"; // Exibe o modal
+    }
+
+    document.querySelector(".close").addEventListener("click", function() {
+        document.getElementById("myModal").style.display = "none";
+    });
+
+    form.addEventListener('submit', async (event) => {
+        event.preventDefault();
+
+        showLoader();
+
+        const nome = document.getElementById('nome').value;
+        const nomeSocial = document.getElementById('nomeSocial').value;
+        const cpf = document.getElementById('cpf').value;
+        const dataNascimento = document.getElementById('dataNascimento').value;
+        const email = document.getElementById('email').value;
+        const telefone = document.getElementById('telefone').value;
+        const cep = document.getElementById('cep').value;
+        const senha = document.getElementById('senha').value;
+        const confirmSenha = document.getElementById('confirmSenha').value;
+
+        if (senha !== confirmSenha) {
+            alert('As senhas não correspondem.');
+            hideLoader();
+            return;
+        }
+
+        const data = {
+            nome: nome,
+            nomeSocial: nomeSocial,
+            cpf: cpf,
+            dataNascimento: dataNascimento,
+            cep: cep,
+            telefone: telefone,
+            email: email,
+            senha: senha
+        };
+        
+
+        try {
+            const response = await fetch('http://localhost:8090/api/usuario', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+
+            if (response.ok) {
+                const result = await response.json();
+                console.log('Success:', result);
+                hideLoader();
+                showModal();
+                // Redirecione ou mostre uma mensagem de sucesso
+            } else {
+                const error = await response.json();
+                console.error('Error:', error);
+                // Mostre uma mensagem de erro para o usuário
+            }
+        } catch (error) {
+            console.error('Fetch error:', error);
+            // Mostre uma mensagem de erro para o usuário
+        }
     });
 });
